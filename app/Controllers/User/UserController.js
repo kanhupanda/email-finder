cec58eab28f032542f3b385b01b2581a6040d3d8
KanhuP <kanhup@rediffmail.com>
2023-08-27 13:10:13 +0530

    updated jsonserver url

--- app/Controllers/User/UserController.js
+++ app/Controllers/User/UserController.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const requestIp = require("request-ip");
 const { JWT_SECRECT } = require("./constants");
-const dbServerUrl = "http://localhost:8912";
+const dbServerUrl = "https://email-finder-db.onrender.com/";
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
